365d7587683d0f29f6cb7f5952ea871451f40c72
microwave-lightspeed <samzaragoza.vcia@gmail.com>
2022-04-04 20:27:58 +0200

    js solved

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-let v_luz;
-let c = 300000;
+let c = 300000.00;
 function getInputValue(){
-    let f = document.getElementById("f").value *10^6;
-    let l_onda = document.getElementById("l_onda").value *10^-2;
+    let f = document.getElementById("f").value *1000000;
+    let l_onda = document.getElementById("l_onda").value /100;
     l_onda = l_onda *2;
     let v = f * l_onda;
-    v_luz = v / 10^-3;
+    let v_luz = v / 1000;
+    v_luz = v_luz.toFixed(2);
     var target = document.getElementById("res");
+    target.innerHTML = " "+v_luz+" Km/s";
     target.innerHTML = " "+v_luz+" Km/s";
     let ea = Math.abs(v_luz-c);
     ea = ea/c * 100;
